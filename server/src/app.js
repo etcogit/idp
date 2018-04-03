66346262384a2866b4674374f20b4b3e9fe101e9
etcocloud9 <cloud9etienne@convie.net>
2018-04-03 18:40:05 +0000

    Suppression de MongoDB au profit d'une db chez mlab.com

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 const app = express()
 app.use(morgan('combine'))
@@ -18,6 +19,35 @@ app.get('/status', (req, res) => {
 // app.set('port', process.env.PORT || 8081)
 
 // app.listen(process.env.PORT || 8081)
+
+// mongoose.connect('mongodb://' + process.env.IP + ':28017/db/idp', function (err, db) {
+var myHost = encodeURIComponent('0.0.0.0:27017/idp')
+console.log(myHost)
+// mongoose.connect('mongodb://' + process.env.IP + ':27017/idp', function (err, db) {
+mongoose.connect('mongodb://etcomlab:etcomlab@ds231559.mlab.com:31559/idp', function (err, db) {
+  if (err) {
+    console.log('Impossible de se connecter à la db', err)
+  } else {
+    console.log('Connexion à la DB effectuée avec succès')
+    var collection = db.collection('users')
+    collection.find({}).toArray(function (err, result) {
+      if (err) {
+        // res.send(err)
+        console.log(err)
+      } else if (result.length) {
+        console.log(result)
+      } else {
+        console.log('No documents found')
+      }
+    })
+  }
+})
+
+/*
+mongoose.connection.once('open', function () {
+  console.log('Connection Estabilished')
+})
+*/
 
 var server = app.listen(8081, function () {
   var host = server.address().address
